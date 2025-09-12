cc11d9025032ec71ea15f9f46117b471eb13b58c
Daniel Gumbs <daniel.r.gumbs@gmail.com>
2025-09-12 18:20:20 +0200

    fix create wedsrijd disabled

--- src/app/game-list/game-list.ts
+++ src/app/game-list/game-list.ts
@@ -1,19 +1,23 @@
-import { Component, Signal } from '@angular/core';
-import { DatePipe } from '@angular/common';
-import { RouterLink } from '@angular/router';
-import { Game, GameService } from '../../services/game.service';
-import { toSignal } from '@angular/core/rxjs-interop';
+import {Component, Signal} from '@angular/core';
+import {AsyncPipe, DatePipe} from '@angular/common';
+import {RouterLink} from '@angular/router';
+import {Game, GameService} from '../../services/game.service';
+import {toSignal} from '@angular/core/rxjs-interop';
+import {Auth, user} from '@angular/fire/auth';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-game-list',
   standalone: true,
-  imports: [RouterLink, DatePipe],
+  imports: [RouterLink, DatePipe, AsyncPipe],
   templateUrl: './game-list.html'
 })
 export class GameList {
   games!: Signal<Game[] | undefined>;
+  user$!: Observable<any>;
 
-  constructor(private gameService: GameService) {
+  constructor(private gameService: GameService, private auth: Auth) {
     this.games = toSignal(this.gameService.getGames());
+    this.user$ = user(this.auth);
   }
 }
