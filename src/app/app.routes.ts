c5dd5f7d7df8a16850c1fadb5b2dcec7739b95fd
Daniel Gumbs <daniel.r.gumbs@gmail.com>
2025-09-26 13:17:08 +0200

    gamedetails

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,6 @@ export const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'games'},
   {path: 'games', loadComponent: () => import('./game-list/game-list').then(m => m.GameList)},
   {path: 'games/new', loadComponent: () => import('./create-game/create-game').then(m => m.CreateGame)},
+  {path: 'games/:id', loadComponent: () => import('./game-detail/game-detail').then(m => m.GameDetailComponent)},
   {path: 'leaderboard', loadComponent: () => import('./leaderboard/leaderboard').then(m => m.LeaderboardComponent)},
 ];
